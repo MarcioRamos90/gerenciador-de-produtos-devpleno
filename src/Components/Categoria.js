16fdedd31b35fc931ab84db3a404bf785a1bec89
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2018-12-08 20:54:24 -0200

    Carregando os dados de produtos do servidor

--- src/Components/Categoria.js
+++ src/Components/Categoria.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Categoria extends Component {
+  state = {
+    produtos: [],
+    categorias: ""
+  };
+  searchCategoria = id => {
+    axios.get(`http://localhost:3001/categoria/${id}`).then(res => {
+      this.setState({
+        categoria: res.data.categoria
+      });
+    });
+  };
+  searchProducts = id => {
+    axios.get(`http://localhost:3001/Produtos?cat=${id}`).then(res => {
+      this.setState({
+        produtos: res.data
+      });
+    });
+  };
+
+  componentDidMount = () => {
+    const { catId } = this.props.match.params;
+
+    this.searchProducts(catId);
+    this.searchCategoria(catId);
+  };
+
+  componentWillReceiveProps = nextProps => {
+    const { catId } = nextProps.match.params;
+    this.searchProducts(catId);
+    this.searchCategoria(catId);
+  };
+
+  renderProdutos = prod => {
+    return <div key={prod.id}>{prod.desc}</div>;
+  };
+
   render() {
     return (
       <div>
-        <h4>Categorias {this.props.match.params.catId}</h4>
+        <h4>{this.state.categoria}</h4>
+        <div>{this.state.produtos.map(this.renderProdutos)}</div>
       </div>
     );
   }
