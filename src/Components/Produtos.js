a7088a78ffc5d1867083e263ae97b5a2d7833e00
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2018-12-08 19:21:13 -0200

    Carregando os dados de categorias do servidor com json-server

--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
+import axios from "axios";
+
 import ProdutosHome from "./ProdutosHome";
 import Categoria from "./Categoria";
 
 class Produtos extends Component {
+  state = {
+    categorias: []
+  };
+
+  componentDidMount = () => {
+    axios.get("http://localhost:3001/categoria").then(res => {
+      this.setState({
+        categorias: res.data
+      });
+    });
+  };
+
+  renderCategorias = cat => {
+    return (
+      <li key={cat.id}>
+        <Link to={`/produtos/categoria/${cat.id}`}>{cat.categoria}</Link>
+      </li>
+    );
+  };
+
   render() {
+    const { categorias } = this.state;
     const { match } = this.props;
     return (
       <div className="row">
         <div className="col-md-3">
           <h3>Categoria</h3>
-          <Link to="produtos/categoria/1">cate 1</Link>
+          {/* TODAS CATEGORIAS */}
+          {categorias.map(this.renderCategorias)}
         </div>
         <div className="col-md-9">
           <h1>Produtos</h1>
